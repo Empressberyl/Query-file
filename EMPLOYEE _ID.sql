7ba7da934709912b7a91fc8352fe9450a0bf83f8
Abigail Oluwafunmilayo Ajayi <speak2abigail@gmail.com>
2023-01-23 22:31:59 +0100

    mysql_query_file

---
+++ EMPLOYEE _ID.sql
@@ -0,0 +1,20 @@
+DELIMITER $$
+
+SELECT CONCAT(' Tunde Faloye ', ' Chisom Gaither ', ' Funmi Adedeji ', 'John Smith ') as "NAME" from employee;
+SELECT CONCAT ( 38 , 27 , 30 , 29 ) as "AGE" from employee;
+SELECT CONCAT (' Logistics ' , ' Human Resources ' , ' Data Analyst ', ' Logistics' ) as "EMP DEPT" from employee; 
+
+END $$
+
+DELIMITER 
+
+
+
+
+
+
+
+
+
+
+
